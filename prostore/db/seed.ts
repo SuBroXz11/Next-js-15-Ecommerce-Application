a9e7085c1442447fbca64b1a18096db19efb62c5
Subrat Karmacharya <subratkrp2@gmail.com>
2025-08-17 12:52:02 +0545

    database seeded with sample users

--- prostore/db/seed.ts
+++ prostore/db/seed.ts
@@ -4,8 +4,13 @@ import sampleData from "./sample-data";
 async function seedDatabase() {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
   await prisma.product.createMany({ data: sampleData.products });
-  console.log("Database seeded with sample products.");
+  await prisma.user.createMany({ data: sampleData.users });
+  console.log("Database seeded with sample users and products.");
 }
 
 seedDatabase();
