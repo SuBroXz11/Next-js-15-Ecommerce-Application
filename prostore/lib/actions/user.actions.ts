87d5cde5118cf984f57a00a0580c65192a40632d
Subrat Karmacharya <subratkrp2@gmail.com>
2025-08-19 21:06:07 +0545

    sign in and sign out server actions created

---
+++ prostore/lib/actions/user.actions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { signInFormSchema } from "../validators";
+import { signIn, signOut } from "@/auth";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+
+// Sign in the user with credentials
+export async function signInUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+    await signIn("credentials", user);
+    return { success: true, message: "Successfully signed in" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { success: false, message: "Invalid credentials" };
+  }
+}
+
+// Sign user out
+export async function signOutUser() {
+  await signOut();
+  return { success: true, message: "Successfully signed out" };
+}
