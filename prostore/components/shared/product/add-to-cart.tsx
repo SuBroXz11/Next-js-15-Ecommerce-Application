c3e1182098c1d69a97424858bb7b60d6ca1cad2f
Subrat Karmacharya <subratkrp2@gmail.com>
2025-08-25 10:09:20 +0545

    add to cart component added

---
+++ prostore/components/shared/product/add-to-cart.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { CartItem } from "@/types";
+import { useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+import { toast } from "sonner";
+import { addItemToCart } from "@/lib/actions/cart.actions";
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter();
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item);
+    if (!res.success) {
+      toast.error(res.message);
+      return;
+    }
+
+    // Handle success add to cart
+    toast.success(`${item.name} added to cart`, {
+      action: {
+        label: "Go to Cart",
+        onClick: () => router.push("/cart"),
+      },
+    });
+  };
+  return (
+    <>
+      <Button className="w-full" type="button" onClick={handleAddToCart}>
+        Add To Cart
+      </Button>
+    </>
+  );
+};
+
+export default AddToCart;
